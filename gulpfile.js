f2d917b0a3de731e1581903dde04ef4997e838e3
buschidos <sergey.snjozyk@gmail.com>
2014-11-28 14:23:24 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var gulp         = require('gulp'),
+
+    compass      = require('gulp-compass'),
+
+    jade         = require('gulp-jade'),
+
+    notify       = require('gulp-notify'),
+    plumber      = require('gulp-plumber'),
+    changed      = require('gulp-changed');
+
+    browserSync  = require('browser-sync'),
+    reload       = browserSync.reload;
+
+//webserver
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: './'
+            // index: 'index.html'
+        },
+        files: ["css/*.css", "*.html", "js/**/*.js"],
+        port: 8080,
+        open: false,
+        notify: false,
+        // proxy: 'yourlocal.dev',
+        // host: '192.168.1.1',
+        // injectChanges: true,
+        // browser: ['google chrome', 'firefox'],
+        ghostMode: {
+            clicks: false,
+            location: false,
+            forms: false,
+            scroll: false
+        }
+    });
+});
+
+//jade
+gulp.task('jade', function() {
+    return gulp.src('jade/*.jade')
+        .pipe(plumber({errorHandler: notify.onError(function(error){return error.message;})}))
+        .pipe(changed('./', {extension: '.html'}))
+        .pipe(jade({pretty: true}))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('jade-all', function() {
+    return gulp.src('jade/*.jade')
+        .pipe(plumber({errorHandler: notify.onError(function(error){return error.message;})}))
+        .pipe(jade({pretty: true}))
+        .pipe(gulp.dest('./'));
+});
+
+
+//compass
+gulp.task('compass', function() {
+    gulp.src('sass/**/*.sass')
+        .pipe(plumber({errorHandler: notify.onError(function(error){return error.message;})}))
+        .pipe(compass({
+            config_file: 'config.rb',
+            css: 'css',
+            sass: 'sass'
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+// watch
+gulp.task('watch', function() {
+    gulp.watch('sass/**/*.sass', ['compass']);
+    gulp.watch('jade/**/*.jade', ['jade']);
+    gulp.watch('jade/includes/*.jade', ['jade-all']);
+    gulp.watch('jade/layouts/*.jade', ['jade-all']);
+});
+
+gulp.task('default',['browser-sync','watch'], function() {});
